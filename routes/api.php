c50e0da2d4d5afa0993ab5a58501be362e25f204
Juliano <julianorafael@gmail.com>
2018-02-13 17:50:57 -0300

    Adding front page document and adding some images to be used on github documentation

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,9 @@
 */
 
 $router->get('/', function () use ($router) {
+    echo '<h1> Voucher Generator <small>by Juliano Barros</small> </h1><br>';
+    echo 'This micro-service app it is to generate vouchers for specific recipients and offers <br>';
+    echo 'You can read all documentation on <a href="https://github.com/juliano-barros/vouchergenerator">Github documentation </a> <br><br>';
     return $router->app->version();
 });
 
